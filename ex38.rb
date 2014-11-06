e4da13d547fbb9cda057261ec6dea5b6692270f0
alexgaganov <alexgaganov@gmail.com>
2014-11-06 18:51:51 +0200

    Fixed mess with exercise numbers.

--- ex38.rb
+++ ex38.rb
@@ -1,61 +1,25 @@
-# create a mapping of state abbreviation
-states = {
-	'Oregon' => 'OR',
-	'Florida' => 'FL',
-	'California' => 'CA',
-	'New York' => 'NY',
-	'Michigan' => 'MI'
-}
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
-# create a basic set of states and some cities in them
-cities = {
-	'CA' => 'San Francisco',
-	'MI' => 'Detroit',
-	'FL' => 'Jacksonville'
-}
+puts "Wait there are not 10 things in that list. Let's fix that."
 
-# add some more cities
-cities['NY'] = 'New York'
-cities['OR'] = 'Portland'
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
-# puts out some cities
-puts '-' * 10
-puts "Michigan's abbreviation is: #{states['Michigan']}"
-puts "Florida's abbreviation is: #{states['Florida']}"
+# using math to make sure there's 10 items
 
-# do it by using the state then cities dict
-puts '-' * 10
-puts "Michigan has: #{cities[states['Michigan']]}"
-puts "Florida has: #{cities[states['Florida']]}"
-
-# puts every state abbreviation
-puts '-' * 10
-states.each do |state, abbrev|
-	puts "#{state} is abbreviated #{abbrev}"
+while stuff.length != 10
+	next_one = more_stuff.pop
+	puts "Adding: #{next_one}"
+	stuff.push(next_one)
+	puts "There are #{stuff.length} items now."
 end
 
-# puts every city in state
-puts '-' * 10
-cities.each do |abbrev, city|
-	puts "#{abbrev} has the city #{city}"
-end
+puts "There we go: #{stuff}"
 
-# now do both at the same time
-puts '-' * 10
-states.each do |state, abbrev|
-	city = cities[abbrev]
-	puts "#{state} is abbreviated #{abbrev} and has city #{city}"
-end
+puts "Let's do some things with stuff."
 
-puts '-' * 10
-# by default ruby says "nil" when something isn't in there
-state = states['Texas']
-
-if !state
-	puts "Sorry, no Texas."
-end
-
-# default values using ||= with the nil result
-city = cities['TX']
-city ||= 'Does Not Exist'
-puts "The city for the state 'TX' is: #{city}"
+puts stuff[1]
+puts stuff[-1] # whoa! fancy
+puts stuff.pop()
+puts stuff.join(' ')
+puts stuff[3..5].join("#")
